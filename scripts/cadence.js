2c82af5dbe61512485d4a679365874e33c759e27
Mohi <momohasapanda@gmail.com>
2026-09-07 21:35:34 +0400

    added form to take input, fixed import, created function for render blocks and made form interactive in js using event listeners and DOM to register form into array

--- scripts/cadence.js
+++ scripts/cadence.js
@@ -1,18 +1,50 @@
 import { initialBlocks } from "../data/blocks.js";
 
-function renderBlocks() {
-  document.querySelectorAll('.day-column').forEach((dayColumn) => {
-    const dayHeader = dayColumn.querySelector('.day-header').textContent;
-    if (dayHeader) {
-      dayHeader === block.day
-      .block-container += `
+const blockForm = document.querySelector("#block-form");
+
+blockForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector("#task-title").value;
+  const day = document.querySelector("#task-day").value;
+  const time = document.querySelector("#task-time").value;
+
+  const newBlock = {
+    title,
+    day,
+    time
+  }
+
+  initialBlocks.push(newBlock);
+  renderBlocks();
+  blockForm.reset();
+  
+});
+
+
+function renderBlocks() { 
+const dayColumns = document.querySelectorAll(".day-column");
+
+dayColumns.forEach((column) => {
+  const container  = column.querySelector(".blocks-container");
+  container.innerHTML = "";
+})
+
+initialBlocks.forEach((block) => {
+  dayColumns.forEach((column) => {
+  const dayHeader = column.querySelector(".day-header").textContent;
+  if (dayHeader === block.day) {
+    const container = column.querySelector(".blocks-container");
+    container.innerHTML += `
       <div class="block-container">
         <strong>${block.time}</strong>
         <p>${block.title}</p>
       </div>
-      `;
-    }
+    `;
+  }
   });
+});
+
 }
 
 renderBlocks();
